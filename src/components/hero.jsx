89436dfd397ad9f18d65cc31bdb5a00c19ee3d41
Matthew Crist <matthew.crist@boston.gov>
2017-10-17 10:44:36 -0400

    Update hero image

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,12 +1,13 @@
 import React from "react"
 
 import styles from "./hero.css"
+import hero from '../images/boston_yes.jpg'
 
 class Hero extends React.Component {
   render() {
     return (
       <div className="b b--fw">
-        <div className="hro hro--t">
+        <div className="hro">
           <div className="hro-c">
             <h1 className="hro-t hro-t--l">Amazon HQ2</h1>
             <div className="hro-st hro-st--l" style={{marginBottom: 25, color: 'rgb(255,255,255)'}}>Some promotional copy goes here</div>
