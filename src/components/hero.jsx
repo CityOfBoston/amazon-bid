43d27bf586196de5b793e13028941f30c16877c8
Matthew Crist <matthew.crist@boston.gov>
2017-10-19 15:27:20 -0400

    Update proposals

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -13,7 +13,7 @@ class Hero extends React.Component {
             <div className="hro-st hro-st--l" style={{marginBottom: 25, color: 'rgb(255,255,255)'}}>Request for proposal response</div>
             <div className="hro-bc">
               <a href="https://d279ml9s9jjbhy.cloudfront.net/BostonAmazonHQ2Read.pdf" className="btn btn--c btn--ib" type='application/pdf'>Read Our Proposal</a>
-              <a href="https://d279ml9s9jjbhy.cloudfront.net/BostonAmazonHQ2Read.pdf" className="btn btn--c btn--ib" download>Download Our Proposal</a>
+              <a href="https://d279ml9s9jjbhy.cloudfront.net/BostonAmazonHQ2.pdf" className="btn btn--c btn--ib" download>Download Our Proposal</a>
             </div>
           </div>
         </div>
