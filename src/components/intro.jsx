9110505d551d2fc30ed648464b60743c44c788ef
Matthew Crist <matthew.crist@boston.gov>
2017-10-19 13:20:26 -0400

    Update intro copy

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -6,9 +6,9 @@ class Intro extends React.Component {
       <div className="b b--fw">
         <div className="b-c">
           <div className="sh">
-            <h2 className="sh-title">Boston Proudly Presents</h2>
+            <h2 className="sh-title">Say yes to Boston</h2>
           </div>
-          <p className="t--intro">Our response to Amazon's HQ2 Request for Proposal. Boston is an open and welcoming city, a city everyone can call home. Take a look at the below featured videos that showcases our City - from innovation and entrepreneurship to development sites and our communities - this is Boston. Yes.</p>
+          <p className="t--intro">We’re proud to present our response to Amazon's HQ2 Request for Proposal. Boston is an open and welcoming city — a City everyone can call home. Whether you’re an innovator, entrepreneur, or a dreamer, Boston has a place for you. Take a look at the featured videos below to learn more about what we have to offer.</p>
         </div>
       </div>
     )
