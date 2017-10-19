c9ca48a30452a043b5b191f446733e1c2ae8e1d7
Matthew Crist <matthew.crist@boston.gov>
2017-10-19 14:09:38 -0400

    Use S3 for PDFs

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -8,7 +8,7 @@ class Intro extends React.Component {
           <div className="sh">
             <h2 className="sh-title">Say yes to Boston</h2>
           </div>
-          <p className="t--intro">We’re proud to present our response to Amazon's HQ2 Request for Proposal. Boston is an open and welcoming city — a City everyone can call home. Whether you’re an innovator, entrepreneur, or a dreamer, Boston has a place for you. Take a look at the featured videos below to learn more about what we have to offer.</p>
+          <p className="t--intro">We’re proud to present our response to Amazon's HQ2 Request for Proposal. Boston is an open and welcoming city — a city everyone can call home. Whether you’re an innovator, entrepreneur, or a dreamer, Boston has a place for you. Take a look at the featured videos below to learn more about what we have to offer.</p>
         </div>
       </div>
     )
