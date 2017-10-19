9c8c44c452b67bcdaea1c4ddfb2eb91a9ebae34c
Matthew Crist <matthew.crist@boston.gov>
2017-10-19 14:07:31 -0400

    Use S3 for PDFs

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,7 +3,6 @@ import React from "react"
 import styles from "./hero.css"
 import hero from '../images/boston_yes.jpg'
 import downlaod from '../proposals/BostonAmazonHQ2.pdf'
-import read from '../proposals/BostonAmazonHQ2Read.pdf'
 
 class Hero extends React.Component {
   render() {
@@ -14,8 +13,8 @@ class Hero extends React.Component {
             <h1 className="hro-t hro-t--l">Amazon HQ2</h1>
             <div className="hro-st hro-st--l" style={{marginBottom: 25, color: 'rgb(255,255,255)'}}>Request for proposal response</div>
             <div className="hro-bc">
-              <a href={read} className="btn btn--c btn--ib" type='application/pdf'>Read Our Proposal</a>
-              <a href={downlaod} className="btn btn--c btn--ib" download>Download Our Proposal</a>
+              <a href="https://s3.us-east-2.amazonaws.com/boston-amazon-pdfs/BostonAmazonHQ2Read.pdf" className="btn btn--c btn--ib" type='application/pdf'>Read Our Proposal</a>
+              <a href="https://s3.us-east-2.amazonaws.com/boston-amazon-pdfs/BostonAmazonHQ2Read.pdf" className="btn btn--c btn--ib" download>Download Our Proposal</a>
             </div>
           </div>
         </div>
