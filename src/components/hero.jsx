5c7ffd32ac563a0d67af299edcd604ef2bf5c9e6
Matthew Crist <matthew.crist@boston.gov>
2017-10-19 13:54:53 -0400

    Change iframe to embed

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -14,7 +14,7 @@ class Hero extends React.Component {
             <h1 className="hro-t hro-t--l">Amazon HQ2</h1>
             <div className="hro-st hro-st--l" style={{marginBottom: 25, color: 'rgb(255,255,255)'}}>Request for proposal response</div>
             <div className="hro-bc">
-              <a href={read} className="btn btn--c btn--ib">Read Our Proposal</a>
+              <a href={read} className="btn btn--c btn--ib" type='application/pdf'>Read Our Proposal</a>
               <a href={downlaod} className="btn btn--c btn--ib" download>Download Our Proposal</a>
             </div>
           </div>
