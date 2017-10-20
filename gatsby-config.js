c12c861368b5f41d52c8e922e6037bf5c5d550c6
Matthew Crist <matthew.crist@boston.gov>
2017-10-20 10:52:43 -0400

    Update favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,14 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
-  plugins: [`gatsby-plugin-react-helmet`, {
-    resolve: `gatsby-plugin-google-analytics`,
-    options: {
-      trackingId: 'UA-99773468-5',
-    },
-  }],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-favicon`, 
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-99773468-5',
+      },
+    }
+  ],
 }
