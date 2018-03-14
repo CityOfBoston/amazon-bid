c0af1e33643739a05b1df117f3337a17f887f1dc
Hondo <hi@hondo.co>
2018-03-13 21:39:37 -0400

    Adds somerville

--- src/pages/rfi.js
+++ src/pages/rfi.js
@@ -11,6 +11,8 @@ import ModalVideo from 'react-modal-video'
 import Intro from '../components/intro'
 
 import styles from "./index.css"
+import pdf from '../images/pdf.svg'
+import zip from '../images/zip.svg'
 
 class IndexPage extends React.Component {
 
@@ -53,6 +55,67 @@ class IndexPage extends React.Component {
           </div>
         </div>
         <GridRFI />
+        <div className="b b--fw">
+          <div className="hro hro--s">
+            <div className="hro-c">
+              <h1 className="hro-t hro-t--l">&nbsp;</h1>
+              <div className="hro-st hro-st--l" style={{marginBottom: 25, color: 'rgb(255,255,255)'}}>&nbsp;</div>
+              <div className="hro-bc">&nbsp;</div>
+            </div>
+          </div>
+        </div>
+        <div className="b b--g b--fw">
+          <div className="b-c">
+            <div className="sh m-b500">
+              <h2 className="sh-title">Somerville</h2>
+            </div>
+            <div className="g m-b500">
+              <div className="g--8">
+                <div className="t--intro">Intro text for Somerville goes here.</div>
+              </div>
+              <div className="g--4 ta-r--large">
+                <a href="https://d279ml9s9jjbhy.cloudfront.net/somerville/somerville.zip" className="btn" download>Download All</a>
+              </div>
+            </div>
+            <div className="g">
+              <a href="https://d279ml9s9jjbhy.cloudfront.net/somerville/MA_Boston_Somerville Orange Line_Talent.pdf" className="cd m-t500 g--12" download>
+                <img src={pdf} alt="City of Boston ❤︎ Amazon" className="lo-i" className="cd-i" width="39" height="59" />
+                <div className="cd-c">
+                  <div className="cd-t" style={{maxWidth: '80%'}}>Talent</div>
+                  <div className="cd-st t--upper t--subtitle"></div>
+                </div>
+              </a>
+              <a href="https://d279ml9s9jjbhy.cloudfront.net/somerville/MA_Boston_Somerville Orange Line_Growth.pdf" className="cd m-t500 g--12" download>
+                <img src={pdf} alt="City of Boston ❤︎ Amazon" className="lo-i" className="cd-i" width="39" height="59" />
+                <div className="cd-c">
+                  <div className="cd-t" style={{maxWidth: '80%'}}>Growth</div>
+                  <div className="cd-st t--upper t--subtitle"></div>
+                </div>
+              </a>
+              <a href="https://d279ml9s9jjbhy.cloudfront.net/somerville/real_estate.zip" className="cd m-t500 g--12" download>
+                <img src={zip} alt="City of Boston ❤︎ Amazon" className="lo-i" className="cd-i" width="39" height="59" />
+                <div className="cd-c">
+                  <div className="cd-t" style={{maxWidth: '80%'}}>South Station</div>
+                  <div className="cd-st t--upper t--subtitle"></div>
+                </div>
+              </a>
+              <a href="https://d279ml9s9jjbhy.cloudfront.net/somerville/MA_Boston_Somerville Orange Line_Taxes and Tax Policy.pdf " className="cd m-t500 g--12" download>
+                <img src={pdf} alt="City of Boston ❤︎ Amazon" className="lo-i" className="cd-i" width="39" height="59" />
+                <div className="cd-c">
+                  <div className="cd-t" style={{maxWidth: '80%'}}>Taxes and Tax Policy</div>
+                  <div className="cd-st t--upper t--subtitle"></div>
+                </div>
+              </a>
+              <a href="https://d279ml9s9jjbhy.cloudfront.net/somerville/MA_Boston_Somerville Orange Line_Incentives.pdf" className="cd m-t500 g--12" download>
+                <img src={pdf} alt="City of Boston ❤︎ Amazon" className="lo-i" className="cd-i" width="39" height="59" />
+                <div className="cd-c">
+                  <div className="cd-t" style={{maxWidth: '80%'}}>Incentives</div>
+                  <div className="cd-st t--upper t--subtitle"></div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
         <Contact />
         <ModalVideo channel='youtube' isOpen={this.state.isOpen} videoId={this.state.videoId} onClose={() => this.setState({isOpen: false})} />
       </div>
